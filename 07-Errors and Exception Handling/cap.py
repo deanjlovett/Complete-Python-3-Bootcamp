bb6153da641889cf43496f0241d79d4895699deb
Dean Lovett <dean.lovett@gmail.com>
2023-12-19 17:47:55 -0800

    add errors & exceptions excercises

--- 07-Errors and Exception Handling/cap.py
+++ 07-Errors and Exception Handling/cap.py
@@ -1,2 +1,11 @@
 def cap_text(text):
-    return text.title()  # replace .capitalize() with .title()
+    # return text.title()  # replace .capitalize() with .title()
+
+    # first pass
+    #return text.capitalize() 
+
+    # second pass
+    retv = []
+    for w in text.split(" "):
+        retv.append(w.capitalize())
+    return ' '.join(retv)
